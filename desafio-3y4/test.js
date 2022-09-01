ce262d2d42a7084f684218e1421fb59257030647
jp-quintana <juanquintana1996@gmail.com>
2022-08-31 23:08:29 -0300

    Add Desafio 3 y 4

---
+++ desafio-3y4/test.js
@@ -0,0 +1,45 @@
+const Contenedor = require('./Contenedor.js');
+
+const productos = new Contenedor('./productos.txt');
+
+// productos.create();
+
+// productos.save({
+//   title: 'Escuadra',
+//   price: 123.45,
+//   thumbnail:
+//     'https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-256.png',
+// });
+
+// productos.save({
+//   title: 'Escuadra',
+//   price: 123.45,
+//   thumbnail:
+//     'https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-256.png',
+// });
+
+// productos.save({
+//   title: 'Escuadra',
+//   price: 123.45,
+//   thumbnail:
+//     'https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-256.png',
+// });
+
+// productos.save({
+//   title: 'Escuadra',
+//   price: 123.45,
+//   thumbnail:
+//     'https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-256.png',
+// });
+
+// productos.getAll();
+
+// productos.getById(3);
+
+// productos.deleteById(2);
+
+// productos.getAll();
+
+// productos.deleteAll();
+
+// productos.getById(3);
