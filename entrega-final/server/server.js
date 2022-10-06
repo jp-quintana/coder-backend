8d8f4f2e04c1218b36f5cc982779517081ed3060
jp-quintana <juanquintana1996@gmail.com>
2022-10-06 19:06:41 -0300

    WIP11: Entrega Final

--- entrega-final/server/server.js
+++ entrega-final/server/server.js
@@ -28,6 +28,10 @@ app.use('/api/productos', adminRoutes);
 app.use('/api/productos', shopRoutes);
 app.use('/api/carrito', cartRoutes);
 
+app.use('*', (req, res, next) => {
+  res.status(404).json({ error: -2, descripcion: 'ruta no implementada' });
+});
+
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
