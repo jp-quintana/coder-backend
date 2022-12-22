a2811fc23a84d89797d5606041eb7c4fe6b3dc95
jp-quintana <juanquintana1996@gmail.com>
2022-12-21 23:51:06 -0300

    WIP: Desafio 33 y 34

---
+++ desafio-33y34/client/src/App.js
@@ -0,0 +1,37 @@
+import { Routes, Route, Navigate } from 'react-router-dom';
+
+import { useAuthContext } from './hooks/useAuthContext';
+
+import Layout from './setup/Layout';
+
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Logout from './pages/Logout';
+import AuthFail from './pages/AuthFail';
+
+import './App.css';
+
+function App() {
+  const { authIsReady } = useAuthContext();
+
+  return (
+    <>
+      {authIsReady && (
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/logout" element={<Logout />} />
+            <Route path="/faillogin" element={<AuthFail />} />
+            <Route path="/failregister" element={<AuthFail />} />
+            <Route path="*" element={<Navigate to="/" />} />
+          </Route>
+        </Routes>
+      )}
+    </>
+  );
+}
+
+export default App;
