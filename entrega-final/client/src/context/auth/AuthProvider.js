dbb994f426e230dcf31b6d72c8cc646d6fd98dd3
jp-quintana <juanquintana1996@gmail.com>
2022-12-28 15:53:49 -0300

    WIP2: Entrega Final

---
+++ entrega-final/client/src/context/auth/AuthProvider.js
@@ -0,0 +1,52 @@
+import { useReducer, useEffect } from 'react';
+
+import AuthContext from './auth-context';
+
+const initialState = { user: null, authIsReady: false };
+
+const authReducer = (state, action) => {
+  switch (action.type) {
+    case 'LOAD_USER': {
+      return { user: action.payload, authIsReady: true };
+    }
+    case 'LOGIN': {
+      return { ...state, user: action.payload };
+    }
+    case 'LOGOUT': {
+      return { ...state, user: null };
+    }
+    default:
+      return state;
+  }
+};
+
+const AuthProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(authReducer, initialState);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const response = await fetch('/user', {
+        credentials: 'include',
+      });
+
+      const data = await response.json();
+
+      if (!data.email) {
+        dispatch({ type: 'LOAD_USER', payload: null });
+        return;
+      }
+
+      dispatch({ type: 'LOAD_USER', payload: data.email });
+    };
+
+    fetchUser();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ ...state, dispatch }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
